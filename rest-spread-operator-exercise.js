fd88a65db7c8e4f44b708e2b8e5010c958ff2bae
Daniel Bae <danibae91@icloud.com>
2021-02-15 14:14:51 -0800

    second commit

--- rest-spread-operator-exercise.js
+++ rest-spread-operator-exercise.js
@@ -62,43 +62,111 @@ doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 
 // Slice and Dice!
+// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
+// Make sure that you are always returning a new array or object and not modifying the existing inputs.
+
 /** remove a random element in the items array
 and return a new array without that item. */
 
 function removeRandom(items) {
+    // initiailze index; look randomNum (items.length)
+    let index = Math.floor(Math.random() * items.length); 
+    // return with slice 
+    return [...items.slice(0, index), ...items.slice(index + 1)]; 
+}
 
+const removeRandom = (items) => {
+    // initiailze index; look randomNum (items.length)
+    let index = Math.floor(Math.random() * items.length); 
+    // return with slice 
+    return [...items.slice(0, index), ...items.slice(index + 1)]; 
 }
 
 /** Return a new array with every item in array1 and array2. */
 
 function extend(array1, array2) {
+    // return [...array1, ...array2]
+    return [...array1, ...array2] 
+}
 
+const extend = (array1, array2) => {
+    // return [...array1, ...array2] 
+    return [...array1, ...array2] 
 }
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 function addKeyVal(obj, key, val) {
-
+    // initialize newObj as rest obj
+    let newObj = {...obj}; 
+    // newObj[key] 
+    newObj[key] = val; 
+    // return 
+    return newObj; 
 }
 
+const addKeyVal = (obj, key, val) => {
+    // initialize newObj as rest obj 
+    let newObj = {...obj}; 
+    // newObj[key] 
+    newObj[key] = val; 
+    // return newObj
+    return newObj; 
+}
 
 /** Return a new object with a key removed. */
 
 function removeKey(obj, key) {
-
+    // initialize newObj as rest obj 
+    let newObj = {...obj}; 
+    // delete key from obj 
+    delete newObj[key]; 
+    // return 
+    return newObj; 
 }
 
+const removeKey = (obj, key) => {
+    // initialize newObj as rest obj 
+    let newObj = {...obj}; 
+    // delete key from obj 
+    delete newObj[key]; 
+    // return obj
+    return newObj; 
+}
 
 /** Combine two objects and return a new object. */
 
 function combine(obj1, obj2) {
-
+    // initialize newObj with rest obj1, obj2 
+    let newObj = {...obj1, ...obj2}; 
+    // return newObj 
+    return newObj; 
 }
 
+const combine = (obj1, obj2) => {
+    // initialize newObj with rest...obj1, obj2
+    let newObj = {...obj1, ...obj2}; 
+    // return obj 
+    return newObj; 
+}
 
 /** Return a new object with a modified key and value. */
 
 function update(obj, key, val) {
+    // initialize newObj with rest {...obj} 
+    let newObj = {...obj}; 
+    // key of newObj 
+    newObj[key] = val; 
+    // return obj
+    return newObj; 
+}
 
+const update = (obj, key, val) => {
+    // initialize newObj with rest {...obj} 
+    let newObj = {...obj}; 
+    // key of newObj 
+    newObj[key] = val; 
+    // return obj
+    return newObj; 
 }
